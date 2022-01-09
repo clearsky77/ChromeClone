6e1725aa6a258e1a5a04fb6c551b00c18df83eb7
clearsky77 <clearsky3264@gmail.com>
2022-01-09 19:54:32 +0900

    객체 작성 및 사용하기

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+
+// 기본 변수
 var oldThing = "" // var은 예전에 썼던 것이다. 이제 쓰지 않도록 한다.
 const a = 2+3; // constant(상수) 변경되지 않는다.
 const myName = "アルニャン";
@@ -23,3 +25,18 @@ console.log(arr);
 
 arr.pop(); // 삭제하기
 console.log(arr);
+
+
+// 객체 사용하기
+const user = {
+    name: "냥냐루",
+    age: 6,
+    fat: false
+};
+
+console.log(user);
+console.log(user.fat);
+
+user.age = 7; // const지만 안의 내용 변경은 에러 발생하지 않는다. 전체 바꾸면 에러 발생. player = 7 의 경우 에러 발생.
+console.log(user.age);
+
