6442efdc91810384ecdf1e6bab9488540d36edfa
clearsky77 <clearsky3264@gmail.com>
2022-01-09 19:29:46 +0900

    상수, 변수 / 타입

--- app.js
+++ app.js
@@ -1,6 +1,14 @@
+var oldThing = "" // var은 예전에 썼던 것이다. 이제 쓰지 않도록 한다.
 const a = 2+3; // constant(상수) 변경되지 않는다.
-const myName = "アルニャン"
+const myName = "アルニャン";
+let youName = "" // let은 변경가능하다.
 
 alert("hi"); // 얼럿
 console.log(myName+"がログインしました。"); // 로그
 console.log(a*2); // 로그
+
+
+const amITall = null;
+let something; // undefined라고 나온다.
+
+console.log(something);
