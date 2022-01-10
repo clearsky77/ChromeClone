1b71754e729b5196925746f2b415bdffa7195276
clearsky77 <clearsky3264@gmail.com>
2022-01-10 10:15:48 +0900

    type을 확인하기. 숫자인지 확인하기.

--- app.js
+++ app.js
@@ -1,19 +1,10 @@
-const calculator = {
-    plus: function(a, b){
-        return a+b;
-    },
-    minus: function(a, b){
-        return a-b;
-    },
-    times: function(a, b){
-        return a*b;
-    },
-    divide: function(a, b){
-        return a/b;
-    },
-    power: function(a, b){
-        return a**b;
-    }
-}
+const age = prompt("おいくつですか。"); // prompt는 옛날 방식이라고 한다.
+console.log(typeof age, parseInt(age)); // typeof는 타입을 알 수 있다.
 
-console.log(calculator.power(2,5));
+
+const age2 = parseInt(prompt("おいくつですか。"));
+console.log(isNaN(age)); // 숫자가 아닌지 반환
+
+if(isNaN(age) || age < 0){
+    console.log("数字を入力してください。");
+}
