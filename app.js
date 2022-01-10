dcc688194f5e95eb9898e1cc1ed97d7e0928191c
clearsky77 <clearsky3264@gmail.com>
2022-01-10 17:05:38 +0900

    html의  form기능을 활용하여 유효성 검사  - 브라우저에서 에러가 보이는 데 차후 해결하기로 함.

--- app.js
+++ app.js
@@ -1,7 +1,16 @@
-const h1 = document.querySelector("div.hello:first-child h1"); // 이렇게도 가능하다.
+const loginInput = document.querySelector("#login-form input");
+const loginButton = document.querySelector("#login-form button");
 
-function handleTitleClick(){
-    h1.classList.toggle("clicked"); // 단 한 줄로 해결
+function onLoginBtnClick(){
+    console.log(loginInput.value);
+    const userName = loginInput.value;
+    // if(userName === ""){
+    //     alert("이름을 입력하세요.")
+    // }else if(userName.length > 15){
+    //     alert("이름은 15자 이내로 입력해주세요.") // html에 maxlength="15"를 하면 제한된다.
+    // }
+    
 }
 
-h1.addEventListener("click", handleTitleClick);
+loginButton.addEventListener("click", onLoginBtnClick)
+
