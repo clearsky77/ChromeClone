a1e4993ca89f2bea6dcf27c3232e8060f4ef5efd
clearsky77 <clearsky3264@gmail.com>
2022-01-10 11:35:22 +0900

    CSS 형식으로 element 찾기  - document.querySelector

--- app.js
+++ app.js
@@ -1,8 +1,14 @@
-const title = document.getElementById("title");
-console.dir(title); // id가 title인 h1의 내부를 볼 수 있다.
-console.log(title.textContent); // id가 title인 h1의 text 내용
 
-title.innerText = "Got you!";
 
-console.log(title.id);
-console.log(title.className);
+ // CSS형식으로 찾기. .hello라는 class에 h1을 가져온다.
+const title = document.querySelector(".hello h1"); // 첫번째 element만 가져온다.
+const titleAll = document.querySelectorAll(".hello h1"); // 모든 element를 가져온다.
+
+const titleById = document.querySelector("#titleById");
+// const titleById = document.getElementById("titleById"); // 위와 이 라인은 같다. 그러나 getElementById로는 하위의 다른 것을 불러올 수 없다.
+
+// document.getElementById("hello"); // 이 방식은 class name이 hello인 것을 알아서 찾아오지만 헷갈린다.
+
+console.log(title);
+console.log(titleAll);
+console.log("titleById: "+titleById)
