4767f69f2012d0607a8f15cde0006f16c73baf18
clearsky77 <clearsky3264@gmail.com>
2022-01-10 16:14:58 +0900

    toggle 하나로 긴 코드를 줄여보자.

--- app.js
+++ app.js
@@ -1,12 +1,7 @@
 const h1 = document.querySelector("div.hello:first-child h1"); // 이렇게도 가능하다.
 
 function handleTitleClick(){
-    const clickedClass = "clicked"
-    if(h1.classList.contains(clickedClass)){ // 만약 포함되어있으면
-        h1.classList.remove(clickedClass); // 지운다.
-    } else {
-        h1.classList.add(clickedClass);
-    }
+    h1.classList.toggle("clicked"); // 단 한 줄로 해결
 }
 
 h1.addEventListener("click", handleTitleClick);
