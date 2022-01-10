044cfaf66b327138b74fcb02b49442731958db1c
clearsky77 <clearsky3264@gmail.com>
2022-01-10 10:53:44 +0900

    getElementById 사용 하기

--- app.js
+++ app.js
@@ -1,10 +1,8 @@
-const age = prompt("おいくつですか。"); // prompt는 옛날 방식이라고 한다.
-console.log(typeof age, parseInt(age)); // typeof는 타입을 알 수 있다.
+const title = document.getElementById("title");
+console.dir(title); // id가 title인 h1의 내부를 볼 수 있다.
+console.log(title.textContent); // id가 title인 h1의 text 내용
 
+title.innerText = "Got you!";
 
-const age2 = parseInt(prompt("おいくつですか。"));
-console.log(isNaN(age)); // 숫자가 아닌지 반환
-
-if(isNaN(age) || age < 0){
-    console.log("数字を入力してください。");
-}
+console.log(title.id);
+console.log(title.className);
