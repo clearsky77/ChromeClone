834ae673cad584eee6e3a3260bd6a6961a424512
clearsky77 <clearsky3264@gmail.com>
2022-01-11 23:51:54 +0900

    클릭 이벤트 정보 확인, 진행 막아보기  - .preventDefault()

--- app.js
+++ app.js
@@ -1,15 +1,21 @@
 const loginForm = document.querySelector("#login-form");
 const loginInput = document.querySelector("#login-form input");
+const link = document.querySelector("a");
 
 function onLoginSubmit(event){ // submit 이벤트 후엔 자동으로 값을 넘겨준다
-    event.preventDefault(); // 이벤트 실행을 막는다. 새로고침 안됨.
+    event.preventDefault(); // 이벤트 진행을 막는다. 자동 새로고침 안됨.
     console.log(event);
     // const userName = loginInput.value;
     // console.log(userName);
 }
 
-loginForm.addEventListener("submit", onLoginSubmit); // 엔터나 버튼시 동작 -> 자동 새로고침
+function handleLinkClick(event){ // 오브젝트를 넘겨준다.
+    event.preventDefault(); // 이벤트 진행을 막는다.
+    console.log(event);
+}
 
+loginForm.addEventListener("submit", onLoginSubmit); // 엔터나 버튼시 동작 -> 자동 새로고침
+link.addEventListener("click",handleLinkClick);
 
 
 
