29191b1db4774cff3588a03539c52448db7fbfd1
clearsky77 <clearsky3264@gmail.com>
2022-01-12 00:30:06 +0900

    CSS의 hidden 사용해보기

--- app.js
+++ app.js
@@ -1,21 +1,21 @@
 const loginForm = document.querySelector("#login-form");
 const loginInput = document.querySelector("#login-form input");
-const link = document.querySelector("a");
+const greeting = document.querySelector("#greeting");
+
+const HIDDEN_CLASSNAME = "hidden";
 
 function onLoginSubmit(event){ // submit 이벤트 후엔 자동으로 값을 넘겨준다
     event.preventDefault(); // 이벤트 진행을 막는다. 자동 새로고침 안됨.
-    console.log(event);
-    // const userName = loginInput.value;
-    // console.log(userName);
-}
+    const username = loginInput.value;
+    console.log(username);
 
-function handleLinkClick(event){ // 오브젝트를 넘겨준다.
-    event.preventDefault(); // 이벤트 진행을 막는다.
-    console.log(event);
+    loginForm.classList.add("hidden");
+    greeting.innerText = `${username}さん、こんにちは!`; // 아래와 같다. 백틱.
+    // greeting.innerText = username+"さん、こんにちは!";
+    greeting.classList.remove(HIDDEN_CLASSNAME);
 }
 
 loginForm.addEventListener("submit", onLoginSubmit); // 엔터나 버튼시 동작 -> 자동 새로고침
-link.addEventListener("click",handleLinkClick);
 
 
 
