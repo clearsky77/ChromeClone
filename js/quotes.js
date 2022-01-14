1fc5741d19bf9b762f7544476f6c31d13eeae3de
clearsky77 <clearsky3264@gmail.com>
2022-01-14 19:59:31 +0900

    Random 활용하여  Random quotes 표시

---
+++ js/quotes.js
@@ -0,0 +1,31 @@
+const quotes = [
+    {
+        quote: "We will find a way, we always have.",
+        author: "Interstella"
+    },
+    {
+        quote: "Nothing is permanent in this wicked world - not even our troubles.",
+        author: "Charlie Chaplin"
+    },
+    {
+        quote: "You may delay, but time will not.",
+        author: "Benjamin Franklin"
+    }
+]
+
+
+const txtQuote = document.querySelector("#quote span:first-child");
+const txtAuthor = document.querySelector("#author span:last-child");
+const txtTodaysQuote = quotes[Math.floor(Math.random()*(quotes.length))];
+
+console.log(quotes[0]);
+
+txtQuote.innerText = txtTodaysQuote.quote;
+txtAuthor.innerText = txtTodaysQuote.author;
+
+console.log(Math.floor(Math.random()*(quotes.length)));
+console.log(txtTodaysQuote.quote);
+
+// Math.random // 10까지 랜덤
+// Math.ceil() // 무조건 올림
+// Math.floor(1.99999) => 1 무조건 뒤를 잘라준다.
