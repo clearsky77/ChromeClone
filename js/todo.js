04df8dbe5edff9a46d0ad664787a0da92f1c6611
clearsky77 <clearsky3264@gmail.com>
2022-01-17 21:56:58 +0900

    추가한 li만 삭제하는 기능

--- js/todo.js
+++ js/todo.js
@@ -2,12 +2,25 @@ const toDoForm = document.getElementById("todo-form");
 const toDoInput = document.querySelector("#todo-form input");
 const toDoList = document.getElementById("todo-list");
 
+function deleteToDo(event){
+    console.log(event);
+    console.dir(event.target); // 이러면 내부를 볼 수 있다.
+    console.dir(event.target.parentElement); // 이러면 button 내부의 property를 볼 수 있다.
+    console.dir(event.target.parentElement.innerText); // 이러면 부모인 li 안의 텍스트 보기 가능.
+
+    const li = event.target.parentElement;// 삭제해야할 li
+    li.remove(); // 삭제
+}
 
 function paintToDo(newTodo){
-    const li = document.createElement("li"); // li 엘레먼트를 생성
-    const span = document.createElement("span"); // li 엘레먼트를 생성
-    li.appendChild(span); // li가 span을 자식으로 둔다.
+    const li = document.createElement("li"); // li 엘리먼트를 생성
+    const span = document.createElement("span"); // span 엘리먼트를 생성
+    const button = document.createElement("button");
+    button.innerText = "❌";
+    button.addEventListener("click",deleteToDo); // 삭제 이벤트를 걸어준다.
     span.innerText = newTodo;
+    li.appendChild(span); // li가 span을 자식으로 둔다.
+    li.appendChild(button);
     toDoList.appendChild(li);
 }
 
