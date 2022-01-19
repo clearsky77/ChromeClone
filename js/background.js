7614b5a8ebf50dce1be54a365b2ac3614f8da463
clearsky77 <clearsky3264@gmail.com>
2022-01-19 22:27:35 +0900

    Background 조정, 시계 크기 조정

--- js/background.js
+++ js/background.js
@@ -1,10 +1,12 @@
-const imgs = ["img_wallpaper1.png","img_wallpaper2.jpg"];
+const imgs = ["img_wallpaper1.png", "img_wallpaper2.jpg"];
 
-
-const seletedImg = imgs[Math.floor(Math.random()*(imgs.length))];
+const selectedImg = imgs[Math.floor(Math.random() * imgs.length)];
 
 const bgImg = document.createElement("img"); // html에 <img> 를 생성한다.
-bgImg.src = `img/${seletedImg}`;
-document.body.appendChild(bgImg); // html에 얹는다.
+// bgImg.src = `img/${seletedImg}`;
+// bgImg.class = "back_img";
+// document.body.appendChild(bgImg); // html에 얹는다.
 
+document.body.style.backgroundImage = `url('img/${selectedImg}')`;
+// document.body.style.backgroundSize = "cover";
 console.log(bgImg);
